0b5eb33841780fbd648c1b3fefe7e46e651760b9
rezha4 <rezha.bahari@gmail.com>
2024-06-25 09:31:23 +0700

    init express with ts

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Skola with Typescript");
+});
+
+app.listen(port, () => {
+  console.log("app listening on port", port);
+});
